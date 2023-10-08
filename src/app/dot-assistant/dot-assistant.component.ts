0deb02199a549a44ec33188dae1320badd7e1ff6
grazirs <graziribeiro95@gmail.com>
2023-10-08 11:48:47 +0200

    add dot assistant component

---
+++ src/app/dot-assistant/dot-assistant.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dot-assistant',
+  templateUrl: './dot-assistant.component.html',
+  styleUrls: ['./dot-assistant.component.scss']
+})
+export class DotAssistantComponent {
+
+}
