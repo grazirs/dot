2a2b2f1cf5773a42f2e7a44020090b6c1bbfd042
grazirs <graziribeiro95@gmail.com>
2023-10-10 12:12:14 +0200

    add resize function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dot';
-  ngOnInit() {
+  onResize() {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
   }
 }
